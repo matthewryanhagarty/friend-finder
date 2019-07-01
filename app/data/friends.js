2764ce4a97ca96559d83583d5d43334bc883eda6
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-07-01 13:31:12 -0700

    worked on looping through object array.

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,27 +2,27 @@ var musiciansList = [
     {
         name: "Matthew Ryan Hagarty",
         about: "Drummer for Fritz Montana / is just one guy & is having a good time",
-        options: [5, 5, 5, 5, 5, 1, 5, 5, 5, 3]
+        options: [5, 5, 5, 5, 5, 1, 5, 5, 5, 3],
     },
     {
         name: "Zachary Scott Carothers",
         about: "bassist for Portugal. The Man/ party animal",
-        options: [3, 5, 4, 4, 3, 4, 5, 4, 4, 4]
+        options: [3, 5, 4, 4, 3, 4, 5, 4, 4, 4],
     },
     {   
         name: "John Balwin Gourley",
         about: "Guitarist & Vocalist for Portugal. The Man/ quiet, but actions speak louder than words",
-        scores: [1, 3, 3, 3, 4, 5, 5, 3, 5, 1]
+        options: [1, 3, 3, 3, 4, 5, 5, 3, 5, 1],
     },
     {    
         name: "Kevin Carlin Logan",
         about: "Bassist for Fritz Montana/ huge nerd, but extremely intelligent",
-        options: [3, 3, 3, 4, 5, 3, 3, 2, 3, 2]
+        options: [3, 3, 3, 4, 5, 3, 3, 2, 3, 2],
     },
     {  
         name: "Daniel Markowitz",
         about: "Keys for Locus Pocus/ the best musician in the band, but also the most humble",
-        options: [2, 2, 3, 5, 3, 2, 3, 4, 5, 2]
+        options: [2, 2, 3, 5, 3, 2, 3, 4, 5, 2],
     },
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,16 +15,21 @@ app.post("/api/musicians", function(req, res) {
 
     for (var i = 0; i < musiciansList.length; i++) {
 
-        // console.log(musiciansList[i])
+        // console.log(musiciansList[i].options)
         var sum = 0;
-        for (var j = 2; j < musiciansList[i].length; j++) {
 
-            var sum = sum += musiciansList[i][j];
+        for (var j = 0; j < musiciansList[i].options.length; j++) {
+            
+            // console.log(musiciansList[i].options[j])
+
+            sum = sum + musiciansList[i].options[j];
+        
             
         }
 
     }             
-    // console.log(musiciansList[0][j])
+
+    // console.log(musiciansList[i][j])
     console.log(sum)
 
 res.json();   //whatever i am sending goes inside json(). 
