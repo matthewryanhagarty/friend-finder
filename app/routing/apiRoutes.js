50d75b9b5f731039d363d96eda749ad78dead243
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-06-30 23:24:55 -0700

    redoing portions of my options section to not include text in the choices. working on looping through object array.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,10 +6,28 @@ app.get("/api/musicians", function(req, res) {
     res.json(musiciansList);
 });
 
+
 app.post("/api/musicians", function(req, res) {
 
     musiciansList.push(req.body);
-    res.json(true); 
+
+
+    for (var i = 0; i < musiciansList.length; i++) {
+
+        // console.log(musiciansList[i])
+
+
+
+        console.log(musiciansList[i].mcdonalds)
+
+        // for (var j = 2; j < items; j++) {
+
+            // console.log(items, sub)
+
+        // }
+    }
+
+res.json();   //whatever i am sending goes inside json(). 
 
 })
 };
