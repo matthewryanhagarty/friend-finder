3772b1094ab13a9b1cf6d3c60fb52ea1369dee94
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-07-01 20:57:20 -0700

    adding styling to website

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,7 +9,7 @@ app.get("/api/musicians", function(req, res) {
 
 app.post("/api/musicians", function(req, res) {
 
-    res.json(true);
+    // res.json(true);
 
     var newMusician = req.body.options;
 
@@ -32,19 +32,15 @@ app.post("/api/musicians", function(req, res) {
              sum = sum + Math.abs(parseInt(newMusician[j]) - parseInt(musiciansList[i].options[j]));
 
         }
-        difference.push({diff:sum,name:musiciansList[i].name});
+        difference.push({diff:sum,name:musiciansList[i].name, about:musiciansList[i].about});
         // console.log(difference);
 
-        difference.sort(function(a, b) {return a.diff-b.diff});
     }             
-    // console.log(musiciansList[i][j])
-    // console.log(sum)
-    // console.log(difference);
 
+    difference.sort(function(a, b) {return a.diff-b.diff});
     musiciansList.push(req.body)
 
 res.json(difference[0]);
-console.log(difference[0])
 
 })
 };
