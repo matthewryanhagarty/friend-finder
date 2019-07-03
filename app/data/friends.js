052a28262b6a921eebb86ab7301646ce3026b7eb
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-07-02 21:42:04 -0700

    added photos that render for matches. resized those photos. added new bios.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,28 +1,32 @@
 var musiciansList = [
     {
         name: "Matthew Ryan Hagarty",
-        about: "Drummer for Fritz Montana / is just one guy & is having a good time",
+        photo: "../assets/images/matthew1.jpg",
+        about: "Drummer for Fritz Montana. You're just one guy & you're having a good time",
         options: [5, 5, 5, 5, 5, 1, 5, 5, 5, 3],
     },
     {
         name: "Zachary Scott Carothers",
-        about: "bassist for Portugal. The Man/ party animal",
+        photo: "../assets/images/zach2.jpg",
+        about: "bassist for Portugal. The Man. You're a party animal & that's why we love you",
         options: [3, 5, 4, 4, 3, 4, 5, 4, 4, 4],
     },
     {   
         name: "John Balwin Gourley",
-        about: "Guitarist & Vocalist for Portugal. The Man/ quiet, but actions speak louder than words",
+        photo: "../assets/images/john1.jpg",
+        about: "Guitarist & Vocalist for Portugal. The Man. You're probably quiet, but sometimes actions speak louder than words",
         options: [1, 3, 3, 3, 4, 5, 5, 3, 5, 1],
     },
     {    
         name: "Kevin Carlin Logan",
         photo: "../assets/images/kevin1.jpg",
-        about: "Bassist for Fritz Montana/ huge nerd, but extremely intelligent",
-        options: [3, 3, 3, 4, 5, 3, 3, 2, 3, 2],
+        about: "Bassist for Fritz Montana. You're most likely a huge nerd, but yet, you're extremely intelligent (or so you think)",
+        options: [3, 4, 3, 4, 5, 3, 3, 3, 4, 2],
     },
     {  
         name: "Daniel Markowitz",
-        about: "Keys for Locus Pocus/ the best musician in the band, but also the most humble",
+        photo: "../assets/images/dm2.jpg",
+        about: "Keys for Locus Pocus. You're probably the best musician in your band, but also the most humble",
         options: [2, 2, 3, 5, 3, 2, 3, 4, 5, 2],
     },
 ];
